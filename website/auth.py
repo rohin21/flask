ddb8e7265aae85afe45ef17298cf70caee6d9e2e
rohin <rohinpandey21@gmail.com>
2023-06-21 18:05:45 +0530

    first commit

---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/logout')
+def logout():
+    pass
+
+@auth.route("/sign-up")
+def signup():
+    return render_template('sign_up.html')
+
+
+
